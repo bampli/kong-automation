69f727b07b72342823579d0038acefc545437488
Jose Motta <josemotta@bampli.com>
2022-07-28 18:43:17 -0300

    add exported realm.json

--- infra/kong-k8s/misc/keycloak/keycloak.sh
+++ infra/kong-k8s/misc/keycloak/keycloak.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 kubectl create ns iam
 helm install keycloak bitnami/keycloak --set auth.adminUser=keycloak,auth.adminPassword=keycloak --namespace iam
+
+# helm install keycloak bitnami/keycloak \
+#     --set auth.adminUser=keycloak,auth.adminPassword=keycloak \
+#     --set extraEnvVars= 'KEYCLOAK_EXTRA_ARGS="-Dkeycloak.import=realm.json"' \
+#     --namespace iam
